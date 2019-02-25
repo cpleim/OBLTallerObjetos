474a9f2fa350001445f5d6c1d1857e46f2da9757
Matías Monegal <matias.monegal@hotmail.com>
2019-02-24 22:32:58 -0300

    Aniadido libreria de clases
    +Aniadido libreria de clases
    +Aniadido creador de recetas
    *Reimplementado menu

---
+++ OBLTallerObjetos/AMCGlib/Receta.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AMCGlib
+{
+    public class Receta
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private string elaboracionBody;
+        private int tiempoCoccion;
+        private int cantPorciones;
+        private string imagen;
+        private string urlVideo;
+        private bool medidas = true; //True -> Metrico, Fasle -> Ingles
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+       
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+       
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+        
+        public string ElaboracionBody
+        {
+            get { return elaboracionBody; }
+            set { elaboracionBody = value; }
+        }
+        
+        public int TiempoCoccion
+        {
+            get { return tiempoCoccion; }
+            set { tiempoCoccion = value; }
+        }
+        
+        public int CantPorciones
+        {
+            get { return cantPorciones; }
+            set { cantPorciones = value; }
+        }
+        
+        public string Imagen
+        {
+            get { return imagen; }
+            set { imagen = value; }
+        }
+        
+        public string UrlVideo
+        {
+            get { return urlVideo; }
+            set { urlVideo = value; }
+        }
+
+        public bool Medidas
+        {
+            get { return medidas; }
+            set { medidas = value; }
+        }
+
+    }
+    }
--- OBLTallerObjetos/OBLTallerObjetos/Starter.cs
+++ OBLTallerObjetos/OBLTallerObjetos/Starter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AMCGlib;
 
 namespace OBLTallerObjetos
 {
@@ -9,8 +10,32 @@ namespace OBLTallerObjetos
     {
         static void Main(string[] args)
         {
+           Random rnd = new Random();
             //Console.SetWindowSize(60, 100);
-            Console.WriteLine("--------- AMCG - Menu Principal---------");
+            //var recetasList = new List<Receta>();
+
+            var receta = new Receta();
+            // Inicializa el objeto y mete 2 recetas como preset
+
+            //TODO
+            /*
+             * Mover el for al metodo "Crear Receta"
+             * Convertirlo a objeto y meterlo en una lista
+             */
+            for (int i = 0; i < 2; i++)
+            {
+                receta.Id = rnd.Next(100);
+                receta.Nombre = "Receta" + rnd.Next(100);
+                receta.Descripcion = "DescripcionReceta";
+                receta.ElaboracionBody = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+                receta.TiempoCoccion = rnd.Next(1, 16);
+                receta.CantPorciones = rnd.Next(1, 9);
+                receta.Imagen = "Imagen" + rnd.Next(100) +".png";
+                receta.UrlVideo = "youtube.com";
+                receta.Medidas = true;
+
+            }
+
             int selectedOption = 0;
             do
             {
@@ -20,13 +45,29 @@ namespace OBLTallerObjetos
 
         static public int showMainMenu()
         {
+            Console.WriteLine("--------- AMCG - Menu Principal---------");
             Console.WriteLine();
             Console.WriteLine("1. Ingreso de recetas");
             Console.WriteLine("2. Ver recetas");
             Console.WriteLine("3. Salir");
             Console.WriteLine();
             var result = Console.ReadLine();
-            return Convert.ToInt32(result);
+            var option = Convert.ToInt32(result);
+
+
+            switch (option)
+            {
+                case 4:
+                    Console.WriteLine("Case 4");
+                    Console.Clear();
+                    break;
+                case 5:
+                    Console.WriteLine("Case 5");
+                    break;
+            }
+            
+            return option;
+
         }
     }
 }
